b1dca4ced16348f2b368cb68d53dca9322356007
NicoMunoz909 <nimunoz28@gmail.com>
2024-12-06 12:09:27 -0500

    Cambiar config de json a js

---
+++ config/config.js
@@ -0,0 +1,24 @@
+module.exports = {
+  development: {
+    username: "root",
+    password: "snj627IRM162!",
+    database: "inventario",
+    host: "127.0.0.1",
+    dialect: "mysql",
+    logging: "console log",
+  },
+  test: {
+    username: "root",
+    password: null,
+    database: "database_test",
+    host: "127.0.0.1",
+    dialect: "mysql",
+  },
+  production: {
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOST,
+    dialect: "mysql",
+  },
+};
