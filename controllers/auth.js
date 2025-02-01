b96694451c3416b768a1a0e77d510a0292a72166
NicoMunoz909 <nicolas.munoz@outlook.es>
2025-02-01 18:34:23 -0500

    Implement login and register. Password hashing. Non expiring token

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,9 +1,42 @@
-const register = (req, res) => {
+require("dotenv").config();
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+const { User } = require("../models");
+const { Op } = require("sequelize");
+const { RESPONSE_STATUS } = require("../config/constants.js");
 
+const register = async (req, res) => {
+
+  const hashedPassword = bcrypt.hashSync(req.body.password, 10)
+
+
+  try {
+    const newUser = await User.create({...req.body, password: hashedPassword})
+    res.status(201).json({status: RESPONSE_STATUS.OK, message: "Usuario creado", data: newUser})
+  } catch (error) {
+    res.status(400).json({status: RESPONSE_STATUS.SERVER_ERROR, message: "No se pudo crear el usuario", data: error})
+  }
 }
 
-const login = (req, res) => {
+const login = async (req, res) => {
+  try {
+    const user = await User.findOne({ 
+      where: {
+        username: req.body.username
+      }
+    })
+    if (!user) return res.status(400).json({status: RESPONSE_STATUS.NO_MATCH, message: "Credenciales Incorrectas", data:null});
 
+    const checkPassword = bcrypt.compareSync(req.body.password, user.password)
+    if (!checkPassword) return res.status(400).json({status: RESPONSE_STATUS.NO_MATCH, message: "Credenciales Incorrectas", data:null})
+    
+    const token = jwt.sign(user.dataValues, process.env.PRIVATE_KEY)
+
+    res.status(200).json({status: RESPONSE_STATUS.OK, message: "Login Exitoso", data:token})
+
+  } catch (error) {
+    res.status(400).json({status: RESPONSE_STATUS.SERVER_ERROR, message: "Error en Login", data: error})
+  }
 }
 
 module.exports = {register, login}
--- server.js
+++ server.js
@@ -2,6 +2,7 @@ require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const { checkExistingSerials, validateIds, validateNotAlreadySold, validateFields, validateId } = require("./validations/inventario");
+const { validateToken } = require("./validations/auth")
 const { consulta, entrada, salida, actualizar, eliminar } = require("./controllers/inventario");
 const { register, login } = require("./controllers/auth");
 const { reporte } = require("./controllers/reportes");
@@ -10,16 +11,19 @@ const app = express();
 
 app.use(express.json());
 app.use(cors());
-app.use(express.static("dist"));
 
 //Use Routes
-app.get("/inventario", validateFields, consulta);
-app.post("/inventario", checkExistingSerials, entrada);
-app.put("/inventario",validateIds, validateNotAlreadySold, salida);
-app.patch("/inventario/:id", validateId, validateFields, actualizar)
-app.delete("/inventario/:id", validateId, eliminar)
-app.get("/reporte", reporte);
-app.post("/auth/register", register)
+app.get("/inventario", validateToken, validateFields, consulta);
+app.post("/inventario", validateToken, checkExistingSerials, entrada);
+app.put("/inventario", validateToken,validateIds, validateNotAlreadySold, salida);
+app.patch("/inventario/:id", validateToken, validateId, validateFields, actualizar)
+app.delete("/inventario/:id", validateToken, validateId, eliminar)
+app.get("/reporte", validateToken, reporte);
+app.get("/auth", validateToken, (req, res) => {
+  return res.send("AUTH")
+})
+app.post("/auth/register", register);
+app.post("/auth/login", login);
 
 const port = process.env.PORT || 4001;
 
