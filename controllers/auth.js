c6221c146fbbea5f06da14200ec654bca9e3b9bc
NicoMunoz909 <nicolas.munoz@outlook.es>
2025-02-04 01:12:54 -0500

    Implement token refresh

--- controllers/auth.js
+++ controllers/auth.js
@@ -30,13 +30,36 @@ const login = async (req, res) => {
     const checkPassword = bcrypt.compareSync(req.body.password, user.password)
     if (!checkPassword) return res.status(400).json({status: RESPONSE_STATUS.NO_MATCH, message: "Credenciales Incorrectas", data:null})
     
-    const token = jwt.sign(user.dataValues, process.env.PRIVATE_KEY)
+    const accessToken = jwt.sign(user.dataValues, process.env.PRIVATE_KEY, {expiresIn: "10s"});
+    const refreshToken = jwt.sign(user.dataValues, process.env.REFRESH_KEY, {expiresIn: "7d"});
 
-    res.status(200).json({status: RESPONSE_STATUS.OK, message: "Login Exitoso", data:token})
+    res.cookie("refreshToken", refreshToken, {
+      httpOnly: true,
+      secure: process.env.NODE_ENV === "production", // Only send over HTTPS in production
+      sameSite: "Strict",
+      path: "/auth", // Limits where the cookie is sent
+      maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days in ms (D * H * M * S * MS)
+      domain: null
+    });
+
+    res.status(200).json({status: RESPONSE_STATUS.OK, message: "Login Exitoso", data: accessToken})
 
   } catch (error) {
+    console.log(error)
     res.status(400).json({status: RESPONSE_STATUS.SERVER_ERROR, message: "Error en Login", data: error})
   }
 }
 
-module.exports = {register, login}
+const logout = async (req, res) => {
+  try {
+    // Clear the refresh token cookie
+    res.clearCookie("refreshToken", { path: "/api/auth/refresh-token" });
+
+    res.status(200).json({ message: "Logout successful" });
+
+  } catch (error) {
+    res.status(500).json({ message: "Error logging out", error });
+  }
+};
+
+module.exports = {register, login, logout }
--- validations/auth.js
+++ validations/auth.js
@@ -6,19 +6,54 @@ const validateToken = (req, res, next) => {
   try {
     if (!req.headers.authorization) return res.status(403).json({stats: RESPONSE_STATUS.CREDENTIALS_ERROR, message: "No se encontraron credenciales", data:null})
   
-    const token = req.headers.authorization.split(' ')[1];
-    if (!token) return res.status(403).json({stats: RESPONSE_STATUS.CREDENTIALS_ERROR, message: "No se encontraron credenciales", data:null})
+    const accessToken = req.headers.authorization.split(' ')[1];
+    if (!accessToken) return res.status(403).json({stats: RESPONSE_STATUS.CREDENTIALS_ERROR, message: "No se encontraron credenciales", data:null})
       
-    const decoded = jwt.verify(token, process.env.PRIVATE_KEY);
+    const decoded = jwt.verify(accessToken, process.env.PRIVATE_KEY);
     if (decoded) {
       next();
     } else {
       return res.send("Wrong credentials");
     }
   } catch (error) {
-    if (error.name = "JsonWebTokenError") return res.status(403).json({status: RESPONSE_STATUS.CREDENTIALS_ERROR, message: "Token incorrecto o expirado", data:error})
+    if (error.name === "TokenExpiredError") return refreshToken(req, res, next);
+    console.log(error);
+    if (error.name === "JsonWebTokenError") return res.status(403).json({status: RESPONSE_STATUS.CREDENTIALS_ERROR, message: "Token inválido", data:error})
     res.status(500).json({status: RESPONSE_STATUS.SERVER_ERROR, message: "Internal Server Error", data:error})
   }
 }
+
+const refreshToken = async (req, res, next) => {
+  try {
+    const refreshToken = req.cookies.refreshToken; // Get from cookie
+    if (!refreshToken) return res.status(401).json({status: RESPONSE_STATUS.CREDENTIALS_ERROR, message: "No refresh token provided" });
+
+    // Verify refresh token
+    const decoded = jwt.verify(refreshToken, process.env.REFRESH_KEY)
+
+    if(decoded) {
+      // Generate new access token
+      const newAccessToken = jwt.sign(
+        {
+          id: decoded.id,
+          username: decoded.username,
+          password: decoded.password,
+          name: decoded.name, 
+          surname: decoded.surname
+        },
+        process.env.PRIVATE_KEY,
+        { expiresIn: "10s" }
+      );
+
+      res.setHeader('Acess-Control-Allow-Headers', 'Authorization');
+      res.setHeader("Authorization", `Bearer ${newAccessToken}`);
+      next();
+    }
+    
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({status: RESPONSE_STATUS.SERVER_ERROR, message: "Error refreshing token", data: error });
+  }
+};
 
 module.exports = { validateToken }
