2f05d892e79b0ac9e401472558b159c1b4937bdf
NicoMunoz909 <nicolas.munoz@outlook.es>
2025-02-04 02:07:58 -0500

    PONE BIEN EL ORIGIN INFELIZ

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: ["http://localhost:5173", "https://id-automation-inventario.onrender.com"], // Allow frontend origin
+    origin: ["http://localhost:5173", "https://id-automation-inventario.vercel.app"], // Allow frontend origin
     credentials: true, // Allow sending cookies
   })
 );
