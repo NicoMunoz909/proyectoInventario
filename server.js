774dae330b7a5c4c0972465d872afcfe617bc93f
NicoMunoz909 <nicolas.munoz@outlook.es>
2025-07-23 16:08:39 -0500

    Cambiar url de origen en CORS

--- server.js
+++ server.js
@@ -29,7 +29,7 @@ app.use(
   cors({
     origin: [
       "http://localhost:5173",
-      "https://id-automation-inventario.vercel.app",
+      "https://inventario-app-theta.vercel.app",
     ], // Allow frontend origin
     credentials: true, // Allow sending cookies
   })
